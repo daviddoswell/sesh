53beaf4695f3c2ee939116b4cd9efb6c3f551241
daviddoswell <team@daviddoswell.com>
2024-06-10 18:13:02 -0400

    Initial Commit

---
+++ Sesh/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sesh
+//
+//  Created by David Doswell on 6/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sesh/SeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeshApp.swift
+//  Sesh
+//
+//  Created by David Doswell on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
